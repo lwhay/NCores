c1b187f1f05066794223172583034ea697afe2fb
lwhay <lwh@whu.edu.cn>
2019-07-10 01:28:39 +0800

    adapt boost-1.70

--- src/columnar/ColumnarFile.h
+++ src/columnar/ColumnarFile.h
@@ -28,7 +28,7 @@ public:
     ColumnarFile(string path, BlockManager *blockManager) : filepath(path), bm(blockManager) {}
 
     void create() {
-        assert(filepath != null);
+        //assert(filepath != nullptr);
         /*if (path) {
             filepath = path;
         }*/
@@ -36,7 +36,7 @@ public:
     }
 
     void open() {
-        assert(filepath != null);
+        //assert(filepath != nullptr);
         /*if (path) {
             filepath = path;
         }*/
