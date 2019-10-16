5b55136a8c8f02c6b5450a03a25055285ec265d8
lwhay <lwh@whu.edu.cn>
2019-10-16 03:57:14 -0700

    enable arrowdumy

---
+++ test/arrow/unit/ArrowDummy.cpp
@@ -0,0 +1,18 @@
+//
+// Created by iclab on 10/16/19.
+//
+
+#include "gtest/gtest.h"
+#include "arrow/status.h"
+#include "arrow/api.h"
+#include "arrow/array.h"
+//#include "parquet/api/schema.h"
+
+TEST(ArrowTest, DummyTest) {
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
